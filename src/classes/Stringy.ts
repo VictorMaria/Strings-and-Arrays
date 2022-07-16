9360733ae1f081dc6db6144b61ff4e66471864f4
Victor Ajayi <victor.ajayi@outlook.com>
2022-07-16 19:22:29 +0100

    implements is suffix integrates nyc for test coverage report Update README.md file

--- src/classes/Stringy.ts
+++ src/classes/Stringy.ts
@@ -20,4 +20,22 @@ export default class Stringy {
 
         return refinedPrefix.length === matches.length;
     }
+
+    isSuffix(suffix: string, isCaseSensitive: boolean = false) {
+        let matches = '';
+        const refinedSuffix = isCaseSensitive ? suffix : suffix.toLowerCase();
+        const refinedText = isCaseSensitive ? this.text : this.text.toLowerCase();
+
+        const suffixTextDifference = refinedText.length - refinedSuffix.length;
+
+        for(let index = refinedSuffix.length - 1; index >= 0; index--) {
+            if (refinedSuffix[index] === refinedText[index + suffixTextDifference]) {
+                matches = `${refinedSuffix[index]}${matches}`;
+            } else {
+                return false;
+            }
+        }
+
+        return matches.length === refinedSuffix.length;
+    }
 }
--- src/tests/stringy.spec.ts
+++ src/tests/stringy.spec.ts
@@ -6,7 +6,8 @@ describe('Stringy methods', () => {
   describe('isPrefix', () => {
     const nougat = new Stringy('nougat');
     const NOUgat = new Stringy('NOUgat');
-    const nougatWithTrailingSpace = new Stringy('nougat ')
+    const nougatWithTrailingSpace = new Stringy('nougat ');
+    const choco = new Stringy('choco');
 
     it('This should return false for nuo as in nougat', () => {
       const result = nougat.isPrefix('nuo');
@@ -15,6 +16,11 @@ describe('Stringy methods', () => {
 
     it('This should return false for o as in nougat', () => {
       const result = nougat.isPrefix('o');
+      expect(result).to.equal(false);
+    });
+
+    it('This should return false for chocolate as in choco', () => {
+      const result = choco.isPrefix('chocolate');
       expect(result).to.equal(false);
     });
     
@@ -50,6 +56,65 @@ describe('Stringy methods', () => {
 
     it('This should return true for nou as in NOUgat where case is not sensitive', () => {
       const result = NOUgat.isPrefix('nou');
+      expect(result).to.equal(true);
+    });
+  });
+
+  describe('isSuffix', () => {
+    const cashew = new Stringy('cashew');
+    const cashewWithStartingSpace = new Stringy(' cashew');
+    const casHEW = new Stringy('HEW');
+    const ring = new Stringy('ring')
+
+    it('This should return false for s in as in cashew', () => {
+      const result = cashew.isSuffix('s');
+      expect(result).to.equal(false);
+    });
+
+    it('This should return false for sew as in cashew', () => {
+      const result = cashew.isSuffix('sew');
+      expect(result).to.equal(false);
+    });
+
+    it('This should return false for HEW as in cashew with isCaseSensitive as true', () => {
+      const result = cashew.isSuffix('HEW', true);
+      expect(result).to.equal(false);
+    });
+
+    it('This should return false for pouring as in ring', () => {
+      const result = ring.isSuffix('pouring');
+      expect(result).to.equal(false);
+    });
+
+
+    it('This should return true for w as in cashew', () => {
+      const result = cashew.isSuffix('w');
+      expect(result).to.equal(true)
+    });
+
+    it('This should return true for hew as in cashew', () => {
+      const result = cashew.isSuffix('hew');
+      expect(result).to.equal(true)
+    });
+
+    it('This should return true for cashew as in cashew', () => {
+      const result = cashew.isSuffix('cashew');
+      expect(result).to.equal(true)
+    });
+
+    it('This should return true for cashew as in cashew with space as prefix', () => {
+      const result = cashewWithStartingSpace.isSuffix('cashew');
+      expect(result).to.equal(true)
+    });
+
+
+    it('This should return true for HEW as in casHEW with isCaseSensitive as false', () => {
+      const result = casHEW.isSuffix('HEW');
+      expect(result).to.equal(true);
+    });
+
+    it('This should return true for hew as in casHEW with isCaseSensitive as false', () => {
+      const result = casHEW.isSuffix('hew');
       expect(result).to.equal(true);
     });
   });
