aff45680718aa67010c489a1df22dedd7bc7cb06
Victor Ajayi <victor.ajayi@outlook.com>
2022-08-08 10:29:58 +0100

    implements isPalindrome method

--- src/playground.ts
+++ src/playground.ts
@@ -1,4 +1,7 @@
 import Stringy from './classes/Stringy';
 
-const word = new Stringy('ink');
-console.log(word.isSuffix('pink'));
+//const cashew = new Stringy('cashew');
+//console.log(cashew.isSuffix('cashew'));
+
+const example = new Stringy('Ma is a nun, as I am  .')
+console.log(example.isPalindrome());
